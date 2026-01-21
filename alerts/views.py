54cb0e32a9d9dc6c63b73e65d995999f2b61654c
Nicolas Noé <nicolas@niconoe.eu>
2026-01-21 10:41:24 +0100

    A couple of observations counters on the home page (to check perfs stay acceptable on the long run)

--- alerts/views.py
+++ alerts/views.py
@@ -1,5 +1,45 @@
 from django.shortcuts import render
 
+from .models import Dataset, Observation, Species
+
 
 def home(request):
-    return render(request, 'home.html')
+    # Total observation count
+    total_count = Observation.objects.count()
+
+    # Pick a few species/datasets for filtered counts (first 3 of each)
+    sample_species = list(Species.objects.all()[:3])
+    sample_datasets = list(Dataset.objects.all()[:3])
+
+    species_counts = [
+        {
+            'species': sp,
+            'count': Observation.objects.filter(species=sp).count(),
+        }
+        for sp in sample_species
+    ]
+
+    dataset_counts = [
+        {
+            'dataset': ds,
+            'count': Observation.objects.filter(source_dataset=ds).count(),
+        }
+        for ds in sample_datasets
+    ]
+
+    # Combined filter: species IN (...) AND dataset IN (...)
+    combined_count = None
+    if sample_species and sample_datasets:
+        combined_count = Observation.objects.filter(
+            species__in=sample_species,
+            source_dataset__in=sample_datasets,
+        ).count()
+
+    return render(request, 'home.html', {
+        'total_count': total_count,
+        'species_counts': species_counts,
+        'dataset_counts': dataset_counts,
+        'combined_count': combined_count,
+        'sample_species': sample_species,
+        'sample_datasets': sample_datasets,
+    })
